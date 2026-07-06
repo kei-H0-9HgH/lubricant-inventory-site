8367b90a68b0c68159a05a3c1f9831b7c24ac9e7
kei-H0-9HgH <xx335577xx@gmail.com>
2026-07-06 18:55:10 +0900

    Restore price input step

--- app-version.js
+++ app-version.js
@@ -1,5 +1,5 @@
 (function () {
-  var version = "20260706-165118";
+  var version = "20260706-185429";
   self.LUBRICANT_APP_VERSION = version;
   if (typeof window !== "undefined") {
     window.LUBRICANT_APP_VERSION = version;
